c974cfd9b09b2224ad44e1bddef902e769ec4774
Max van Schoonderwoerd <max@elephantcs.nl>
2025-05-23 15:21:00 +0200

    Improve layout and formatting in Details and app views for better readability

--- resources/views/Details.blade.php
+++ resources/views/Details.blade.php
@@ -5,32 +5,32 @@
     {{--title section--}}
     <section class="section">
         <div class="container">
+            <a href="{{ route('post.index') }}">Back</a>
             <h1>{{$post->title}} | Details</h1>
-            <a href="{{ route('post.index') }}">Home</a>
+            <p>Posted by {{$post->user->name}}</p>
         </div>
     </section>
 
     {{--details section--}}
     <section class="section">
         @if(Auth::id() == $post->user_id)
-            {{--            form view when the current user is the op--}}
+            {{-- form view when the current user is the op--}}
             <div class="container">
-                <form class="form" action="{{route('post.update', $post->id)}}" method="post"
-                      enctype="multipart/form-data">
+                <form class="form" action="{{route('post.update', $post->id)}}" method="post" enctype="multipart/form-data">
                     @csrf
                     @method('PUT')
                     <div class="mb-3">
-                        <input class="form-control" type="text" id="title" name="title" maxlength="40"
-                               placeholder="Title" value="{{$post->title}}">
+                        <input class="form-control" type="text" id="title" name="title" maxlength="40" placeholder="Title"
+                            value="{{$post->title}}">
                     </div>
                     <div class="mb-3">
-                <textarea class="form-control" type="text" id="description" name="description" maxlength="240"
-                          placeholder="Description">{{$post->description}}</textarea>
+                        <textarea class="form-control" type="text" id="description" name="description" maxlength="240"
+                            placeholder="Description">{{$post->description}}</textarea>
                     </div>
                     <div class="form-row">
                         <div class="mb-3">
-                            <input class="form-control" type="number" id="bpm" name="bpm" placeholder="bpm"
-                                   min="30" max="300" value="{{$post->bpm}}">
+                            <input class="form-control" type="number" id="bpm" name="bpm" placeholder="bpm" min="30" max="300"
+                                value="{{$post->bpm}}">
                         </div>
                         <div class="mb-3">
                             <select class="form-control" id="genre" name="genre">
@@ -47,22 +47,23 @@
                                 <option value="Other">Other</option>
                             </select>
                             @error('genre')
-                            <span>{{$message}}</span>
+                                <span>{{$message}}</span>
                             @enderror
                         </div>
                     </div>
                     <button class="btn btn-primary mb-2" type="submit">Edit Beat</button>
                 </form>
-                {{--                delete button--}}
+                {{-- delete button--}}
                 <form action="{{ route('post.destroy', ['post' => $post->id]) }}" method="POST">
                     @csrf
                     @method('DELETE')
                     <button type="submit" class="btn btn-danger btn-block">Delete Beat</button>
                 </form>
+
                 <img class="coverImg" src="{{$post->cover}}" alt="Cover art">
             </div>
         @else
-            {{--            table view when the current user is not the op--}}
+            {{-- table view when the current user is not the op--}}
             <div class="container">
                 <table>
                     <tr>
@@ -95,4 +96,3 @@
         </div>
     </section>
 @endsection
-
